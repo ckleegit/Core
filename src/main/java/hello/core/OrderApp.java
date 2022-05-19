102cc2cd892745e61f85f297ab47beca6411e9fc
ckleegit <ckleex2@naver.com>
2022-05-19 20:07:25 +0900

    20220519.GalaxyBook

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -25,9 +25,5 @@ public class OrderApp {
         System.out.println("order = " + order.getItemPrice());
         System.out.println("DiscountPrice = " + order.getDiscountPrice());
         System.out.println("order.calculatePrice = " + order.calculatePrice());
-        //이창근 테스트
-        //이창근 맥에서 입력함
-        //이창근 윈도우에서 입력함
-        //이창근 갤럭시북에서 입력함 테스트
     }
 }
